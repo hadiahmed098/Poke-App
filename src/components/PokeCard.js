c0a50a6ff195cc208f5f1ace72ca7b7efc6c4c42
adhikary97 <adhikary@purdue.edu>
2020-03-07 19:56:43 -0500

    formatting

--- src/components/PokeCard.js
+++ src/components/PokeCard.js
@@ -14,17 +14,22 @@ const PokeCard = props => {
         style={{ textDecoration: 'none' }}
       >
         <Paper elevation={8} style={{ backgroundColor: '#A9A9A9' }}>
-          <Grid container justify="center" direction="column" alignItems="center">
-          <img
-            src={`https://pokeres.bastionbot.org/images/pokemon/${props.id}.png`}
-            height={300}
-            width={300}
-            alt={props.name}
-          />
-          <Typography variant="h4">
-            {props.name[0].toUpperCase() +
-              props.name.substring(1, props.name.length)}
-          </Typography>
+          <Grid
+            container
+            justify="center"
+            direction="column"
+            alignItems="center"
+          >
+            <img
+              src={`https://pokeres.bastionbot.org/images/pokemon/${props.id}.png`}
+              height={300}
+              width={300}
+              alt={props.name}
+            />
+            <Typography variant="h4">
+              {props.name[0].toUpperCase() +
+                props.name.substring(1, props.name.length)}
+            </Typography>
           </Grid>
         </Paper>
       </Link>
